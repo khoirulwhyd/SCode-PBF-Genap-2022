1a5c9d8a9e8ccd196c19ba34f272e602d79f8a5f
khoirulwhyd <gamingkenzo7@gmail.com>
2022-04-12 21:43:29 +0700

    Praktikum Modul 9 - GLOBAL API

---
+++ Minggu-9/web-reactku/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import BlogPost from "./container/BlogPost/BlogPost";
+
+import Mahasiswa from "./container/Mahasiswa/Mahasiswa";
+
+ReactDOM.render(<Mahasiswa />, document.getElementById('content'));
