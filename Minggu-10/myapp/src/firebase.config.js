73b1155df5198785a50da58f4d036a7c56d791ea
khoirulwhyd <gamingkenzo7@gmail.com>
2022-04-24 22:19:19 +0700

    modul firebase

---
+++ Minggu-10/myapp/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAogKNWWWaZ6mw0Rw7UHWAjQDU33bOo-4E",
+    authDomain: "authproject-bf30e.firebaseapp.com",
+    projectId: "authproject-bf30e",
+    storageBucket: "authproject-bf30e.appspot.com",
+    messagingSenderId: "234831144170",
+    appId: "1:234831144170:web:ff08d2d2b7ef2d6dca2393",
+    measurementId: "G-7KT1VKTXZQ"
+};
